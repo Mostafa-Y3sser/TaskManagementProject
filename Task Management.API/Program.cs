3433506c5fbc69cff4b0626aa187ba115cd0c4ff
Mostafa_Yasser <UG_31202103@ics.tanta.edu.eg>
2025-08-04 03:06:55 +0300

    Register Repositories and UnitOfWork Add Swagger

--- Task Management.API/Program.cs
+++ Task Management.API/Program.cs
@@ -1,4 +1,7 @@
+using Task_Management.Domain.Entities;
+using Task_Management.Domain.Interfaces;
 using Task_Management.Infrastructure.Extensions;
+using Task_Management.Infrastructure.Persistence.Repositories;
 
 namespace Task_Management
 {
@@ -16,18 +19,27 @@ namespace Task_Management
 
             builder.Services.AddInfrastructureServices(builder.Configuration);
 
+            builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+            builder.Services.AddScoped<IRepository<TaskItem>, TaskRepository>();
+            builder.Services.AddScoped<IRepository<Board>, BoardRepository>();
+            builder.Services.AddScoped<IRepository<Project>, ProjectRepository>();
+
+            builder.Services.AddOpenApi();
+            builder.Services.AddSwaggerGen();
+
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.UseSwagger();
+                app.UseSwaggerUI();
             }
 
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
-
 
             app.MapControllers();
 
